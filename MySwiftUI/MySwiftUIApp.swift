732590ba1713959b341adabb4fab341aa90ca5bb
Jash-Balar <jashbalar.dev@gmail.com>
2025-01-24 13:22:32 +0530

    first commit

---
+++ MySwiftUI/MySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUIApp.swift
+//  MySwiftUI
+//
+//  Created by Jash Balar on 24/01/25.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
